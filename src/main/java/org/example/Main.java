5343391196708312f27a4fb93d447b65b4760607
PrinceKumarMaurya591 <72147360+PrinceKumarMaurya591@users.noreply.github.com>
2025-05-24 00:23:12 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import org.example.java8.Java8Basic;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+//Main method 
 public class Main {
     public static void main(String[] args) {
        Java8Basic java8Basic = new Java8Basic();
